196776895b196e174af93f7ba5d7a9b4fe4cfba5
Juan Sáez García <contacto@juamber.com>
2022-02-12 14:51:20 +0100

    INSERT INTO

--- lessons/01.create/script.sql
+++ lessons/01.create/script.sql
@@ -8,5 +8,38 @@ CREATE TABLE `platziblog`.`people` (
   `city` VARCHAR(255) NULL,
   PRIMARY KEY (`id`)
 );
+INSERT INTO
+  `platziblog`.`people` (
+    `id`,
+    `last_name`,
+    `first_name`,
+    `address`,
+    `city`
+  )
+VALUES
+  (
+    '1',
+    'Vásquez',
+    'Israel',
+    'Calle Famosa Num 1',
+    'México'
+  ),
+  (
+    '2',
+    'Hernández',
+    'Mónica',
+    'Reforma 222',
+    'México'
+  ),
+  (
+    '3',
+    'Alanis',
+    'Edgar',
+    'Central 1',
+    'Monterrey'
+  );
 
-SELECT * FROM platziblog.people;
+SELECT
+  *
+FROM
+  platziblog.people;
