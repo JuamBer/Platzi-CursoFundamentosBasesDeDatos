20c45d3ced45af21300d5bcfc23b32d03536029a
Juan Sáez García <juambersaezgarcia@gmail.com>
2022-05-17 21:07:15 +0200

    WHERE NO NULO

--- lessons/08.where/script.sql
+++ lessons/08.where/script.sql
@@ -2,4 +2,13 @@ USE `platziblog`;
 
 SELECT * FROM posts WHERE id < 50
 
-SELECT * FROM posts WHERE status != 'activo' 
+SELECT * FROM posts WHERE estatus != 'activo' 
+
+SELECT * FROM posts WHERE usuario_id IS NULL 
+SELECT * FROM posts WHERE usuario_id IS NOT NULL
+
+SELECT * FROM posts WHERE usuario_id IS NOT NULL 
+    AND estatus = 'activo'
+    AND id < 50
+    AND categoria_id = 2
+    AND YEAR(fecha_publicacion) = 2025
